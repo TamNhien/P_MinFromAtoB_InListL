85bbf871927bc4915d004e6a0cfd7b60308c4a35
TamNhien <canhthien91@gmail.com>
2024-03-27 18:27:45 +0700

    Add project files.

---
+++ P_MinFromAtoB_InListL.py
@@ -0,0 +1,9 @@
+﻿def find_min(L, a, b):
+    if not L: return "Danh sách trống"
+    if not (0 <= a < b <= len(L)): return "Vị trí không hợp lệ"
+    return min(L[a:b+1])
+
+if __name__ == "__main__":
+    L = list(map(int, input("Nhập vào danh sách số nguyên, cách nhau bởi dấu cách: ").split()))
+    a, b = map(int, input("Nhập vào số nguyên dương a và b (a < b < len(L)): ").split())
+    print("Số nhỏ nhất từ vị trí", a, "đến vị trí", b, "là : ", find_min(L, a, b))
